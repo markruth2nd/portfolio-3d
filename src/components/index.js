45e40cf185d428a34836d64011e987660446f278
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-04-05 19:13:09 +0100

    added rafce to all jsx files as well as importing all jsx component files in the index.js file ready for use

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import About from "./About";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+
+
+
+export {
+    About, Contact, Experience, Hero, Navbar
+}
